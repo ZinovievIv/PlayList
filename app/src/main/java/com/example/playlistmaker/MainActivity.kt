b183a4b9989cdee6cfe165dd8f3dc65e67ab5159
zinoviev <zinovev.ivan@mail.ru>
2023-09-28 22:17:44 +0300

    разобрался с ClickListener

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,14 +15,28 @@ class MainActivity : AppCompatActivity() {
         val buttonSettings = findViewById<Button>(R.id.buttonSettings)
         val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Нажали на кнопку 1!", Toast.LENGTH_SHORT).show()
             }
         }
-        val buttonMediaClickListener: View.OnClickListener = View.OnClickListener { Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show() }
-        val buttonSettingClickListener: View.OnClickListener = View.OnClickListener {Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()}
+        val buttonMediaClickListener: View.OnClickListener = View.OnClickListener { Toast.makeText(this@MainActivity, "Нажали на кнопку 2!", Toast.LENGTH_SHORT).show() }
 
         buttonSearch.setOnClickListener(buttonSearchClickListener)
         buttonMedia.setOnClickListener(buttonMediaClickListener)
-        buttonSettings.setOnClickListener(buttonSettingClickListener)
+        buttonSettings.setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.buttonSearch -> {
+                Toast.makeText(this@MainActivity,"Нажата кнопка Поиск", Toast.LENGTH_SHORT).show()
+            }
+            R.id.buttonSettings -> {
+                Toast.makeText(this@MainActivity,"Нажата кнопка Настройки", Toast.LENGTH_SHORT).show()
+            }
+            R.id.buttonMedia -> {
+                Toast.makeText(this@MainActivity,"Нажата кнопка Библиотека", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
